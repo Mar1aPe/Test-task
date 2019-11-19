5c84150b86e627edede1c59579a9c2c4577eeb04
Maria <adresmarii@gmail.com>
2019-11-19 22:22:31 +0100

    Create .js and .scss components for 1st page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import Description from './components/Description';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import ProfilePic from './components/ProfilePic';
 
 function App() {
   return (
