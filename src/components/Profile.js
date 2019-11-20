9745bd4a0aa2762d0d8067b5dc1735776d5e00f4
Maria <adresmarii@gmail.com>
2019-11-20 23:52:22 +0100

    Style Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <div>
-            profile
+        <div className='profile'>
+            <h1>Marilyn Monroe</h1>
+            <p className='location'><i class="fas fa-map-marker-alt"></i> Poznań, PL </p>
+            <br />
+            <br />
+            <div className="quote">
+                <p>You wouldn't hit a man with no trousers on, woud you? Multiply your anger by about a hudred, Kate, that's how much he thinks he loves you. My Lord! You are a tripod. Boxing is about respect.</p>
+                <br />
+                <div className='blockquote'>
+                    When a naked man's chasing a woman through an alley <br />
+                    with a butcher knife and a hard-on, <br />
+                    I figure he's not out collecting for the Red Cross
+            </div>
+                <br />
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur assumenda illum, necessitatibus molestiae hic, porro repellat, rem ducimus non vitae quae consequatur ea reprehenderit! Beatae quis officia accusamus ut error!
+            </p>
+            </div>
         </div>
     );
 }
