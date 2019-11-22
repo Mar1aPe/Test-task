7fb8fe86789f32d81d3b285223cabe8d1830c9d0
Maria <adresmarii@gmail.com>
2019-11-22 02:32:05 +0100

    Updates

---
+++ precache-manifest.95fa9e3ef499c3d6117078e43fd25db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec1c4030475236bce6c00b6f8e82ada",
+    "url": "/Test-task/index.html"
+  },
+  {
+    "revision": "eb3d7048448cde254a6c",
+    "url": "/Test-task/static/css/main.a0977e80.chunk.css"
+  },
+  {
+    "revision": "37daee09cedab9ede5fc",
+    "url": "/Test-task/static/js/2.a7607634.chunk.js"
+  },
+  {
+    "revision": "eb3d7048448cde254a6c",
+    "url": "/Test-task/static/js/main.9e9ac4d3.chunk.js"
+  },
+  {
+    "revision": "37373c1d2e22c506cfa7",
+    "url": "/Test-task/static/js/runtime-main.a3f3c76b.js"
+  },
+  {
+    "revision": "a8e6a1b7ee197e8ad5f762b103ee3685",
+    "url": "/Test-task/static/media/headerpic.a8e6a1b7.JPG"
+  },
+  {
+    "revision": "edb9a0ed8fdac701f8978c31bba7ad5e",
+    "url": "/Test-task/static/media/profilepic.edb9a0ed.JPG"
+  }
+]);
